896724d59e6957fd12a1f36bb447e669cf6d2199
Tahahha <tahainanya@gmail.com>
2025-11-18 15:59:58 +0100

    new code

--- oop/Demo1/Program.cs
+++ oop/Demo1/Program.cs
@@ -1,10 +1,57 @@
-﻿namespace Demo1
+﻿//The concepts of OOP in C#
+using System;
+namespace OOP_in_Csharp
 {
-    internal class Program
+    public class Computer
     {
-        static void Main(string[] args)
+        public string _BIOSname;
+        public string _ipadress;
+        public string _OS;
+
+        public Computer(string bn, string ip, string os)
         {
-            Console.WriteLine("Hello, World!");
+            _BIOSname = bn;
+            _ipadress = ip;
+            _OS = os;
+        }
+    }
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Computer comp1 = new("alfa", "10.0.0.23", "Windows 11");
+            Console.WriteLine(comp1._BIOSname);
         }
     }
 }
+
+/*
+
+           Computer[] net = new Computer[4];
+
+           for (int i = 0; i < net.Length; i++)
+           {
+               net[i] =
+                 new Computer("comp" + i.ToString(),
+                       "10.0." + getNum() + "." + getNum(), "Win10");
+           }
+
+           for (int i = 0; i < net.Length; i++)
+           {
+               Console.WriteLine("{0} {1}", net[i]._BIOSname, net[i]._ipadress);
+           }
+
+           */
+
+/*
+  static string getNum()
+        {
+            Random random = new Random();
+            int num;
+            num = random.Next(1, 255);
+            return num.ToString();
+        }
+ 
+ 
+ */
